3b6167d08943b23af17f1895ea24dfd39b3b903a
Bogdan-Lishchynskiy <lishchynskiy91@gmail.com>
2021-10-19 13:32:35 +0300

    [test] for pre commit

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { http } from '../http-client/http';
 // import api from '../http-client/api'
-import { GitHubRepo } from '../models/github.model'
-
+import { GitHubRepo } from '../models/github.model';
 
 @Injectable()
 export class GithubService {
   async fetchGitHubRepos(): Promise<GitHubRepo[]> {
-    let res = await http.get(`/users/${process.env.GITHUB_USER_NAME}/repos`)
-    console.log('res.data = = =', res.data[0])
+    let res = await http.get(`/users/${process.env.GITHUB_USER_NAME}/repos`);
+    console.log('res.data = = =', res.data[0]);
 
     return res.data[0];
   }
